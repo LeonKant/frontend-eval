67744686bc2633eacfeb606e7f68c28777d778d4
LeonKant <lkantikov20@gmail.com>
2025-04-04 15:54:41 -0700

    init: dice roller page

---
+++ app/dice-roller/page.tsx
@@ -0,0 +1,10 @@
+import PageTemplate from "@/components/PageTemplate";
+import DiceRoller from "./_components/DiceRoller";
+
+export default function DiceRollerPage() {
+  return (
+    <PageTemplate header="Dice Roller">
+      <DiceRoller></DiceRoller>
+    </PageTemplate>
+  );
+}
