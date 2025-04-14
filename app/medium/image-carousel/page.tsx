9adb669e2adffde424aa93635a0ccb0697a44e83
LeonKant <lkantikov20@gmail.com>
2025-04-14 15:09:03 -0700

    init: image carousel page

---
+++ app/medium/image-carousel/page.tsx
@@ -0,0 +1,10 @@
+import PageTemplate from "@/components/PageTemplate";
+import ImageCarousel from "./_components/ImageCarousel";
+
+const ImageCarouselPage = () => (
+  <PageTemplate header="Image Carousel">
+    <ImageCarousel />
+  </PageTemplate>
+);
+
+export default ImageCarouselPage;
